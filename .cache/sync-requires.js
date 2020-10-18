a26510232e649cd9819a816d1deeaaf98440db62
Bipin Rai <bipi0007@edu.easj.dk>
2020-10-19 01:13:04 +0200

    try 1

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\Portfolio\\my-gatsby-project\\.cache\\dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("E:\\Portfolio\\my-gatsby-project\\node_modules\\gatsby-plugin-offline\\app-shell.js"))),
   "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("E:\\Portfolio\\my-gatsby-project\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")))
 }
 

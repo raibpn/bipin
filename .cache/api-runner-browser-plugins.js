db21186385bafa9fb7f75f76d6928dba25b31ebb
Bipin Rai <bipi0007@edu.easj.dk>
2020-10-19 17:10:15 +0200

    sdf

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-theme-ui/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Cara - @lekoarts/gatsby-theme-cara","short_name":"Cara","description":"Playful and Colorful One-Page portfolio featuring Parallax effects and animations","start_url":"/","background_color":"#141821","theme_color":"#f6ad55","display":"standalone","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":null},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]

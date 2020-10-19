e101b562d765b52db251d734e7ead32b4c79f5f2
Bipin Rai <bipi0007@edu.easj.dk>
2020-10-19 17:07:14 +0200

    jpt

---
+++ src/@lekoarts/gatsby-theme-cara/sections/footer.tsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { Box, Flex, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="" variant="">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button" 
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer

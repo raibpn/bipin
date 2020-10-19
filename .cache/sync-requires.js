93748ac3086ec4b213d225726c53c14369bc1851
Bipin Rai <bipi0007@edu.easj.dk>
2020-10-19 17:48:16 +0200

    try

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\Portfolio\\my-gatsby-project\\.cache\\dev-404-page.js"))),
-  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("E:\\Portfolio\\my-gatsby-project\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\bipin\\.cache\\dev-404-page.js"))),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("E:\\bipin\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")))
 }
 
